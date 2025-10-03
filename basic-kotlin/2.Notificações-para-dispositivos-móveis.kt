854194a77b945a09ace615ba00da760a6ecf2222
elder.storck <84104017+elder-storck@users.noreply.github.com>
2025-10-03 14:42:01 -0300

    Create 2.Notificações-para-dispositivos-móveis.kt
    Uso de variáveis, além de condicional if e else

---
+++ basic-kotlin/2.Notificações-para-dispositivos-móveis.kt
@@ -0,0 +1,24 @@
+/**
+ * Normalmente, o smartphone oferece um resumo das notificações.
+ * No código inicial fornecido no snippet abaixo, escreva um programa que mostra a mensagem 
+ * de resumo com base no número de notificações recebidas. A mensagem precisa incluir:
+ * 		O número exato de notificações quando menor que 100.
+ * 		99+ como o número de notificações quando houver 100 ou mais.
+ */
+
+fun main() {
+    val morningNotification = 51
+    val eveningNotification = 135
+    
+    printNotificationSummary(morningNotification)
+    printNotificationSummary(eveningNotification)
+}
+
+
+fun printNotificationSummary(numberOfMessages: Int) {
+    if(numberOfMessages < 100){
+        print("You have " +numberOfMessages+" notifications.\n")
+    }else{
+        print("Your phone is blowing up! You have 99+ notifications.\n")
+    }
+}
